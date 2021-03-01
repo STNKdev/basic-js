51789764989dc25200496b52eec236c0a74e2c24
AleksandrSTNK <aleksandr.stnk@gmail.com>
2021-03-01 14:57:54 +0300

    feat: implement getSeason()

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,37 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+
+// весна — spring, лето — summer, осень — autumn (fall), зима — winter
+
+module.exports = function getSeason(date) {
+  if (date === undefined) {
+    return 'Unable to determine the time of year!';
+  } else if (Object.keys(date).length !== 0) {
+    throw new Error();
+  }
+
+  const [SPRING, SUMMER, AUTUMN, WINTER] = ['spring', 'summer', 'autumn', 'winter'];
+
+  switch (date.getMonth()) {
+    case 0:
+    case 1:
+    case 11:
+      return WINTER;
+
+    case 2:
+    case 3:
+    case 4:
+      return SPRING;
+
+    case 5:
+    case 6:
+    case 7:
+      return SUMMER;
+
+    case 8:
+    case 9:
+    case 10:
+      return AUTUMN;
+  }
+
 };
